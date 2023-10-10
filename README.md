46905f2e52a2470c3e58a6d888d04e5dd66cf109
c4710n <c4710n@users.noreply.github.com>
2023-10-10 21:13:51 +0800

    chore: add Nix flake support

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# DockerZeroman
+# docker-zeroman
+
+![Nix flakes](https://img.shields.io/badge/Nix%20flakes-5277C3?logo=nixos&logoColor=white&style=flat-square)
 
 **TODO: Add description**
 
@@ -18,4 +20,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/docker_zeroman>.
-
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "docker-zeroman";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      overlayBeam = _final: prev: {
+        beam = prev.beam // {
+          interpreters = prev.beam.interpreters // {
+            erlang_26 = prev.beam.interpreters.erlang_26.overrideAttrs (_finalAttrs: prevAttrs: {
+              configureFlags = prevAttrs.configureFlags ++ [ "--disable-jit" ];
+            });
+
+            erlang_25 = prev.beam.interpreters.erlang_25.overrideAttrs (_finalAttrs: prevAttrs: {
+              configureFlags = prevAttrs.configureFlags ++ [ "--disable-jit" ];
+            });
+          };
+        };
+      };
+    in
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          overlays = if system == "aarch64-darwin" then [ overlayBeam ] else [ ];
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [ elixir docker-client ];
+        };
+      }
+    ));
+}
