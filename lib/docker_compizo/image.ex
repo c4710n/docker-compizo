a93e11a460bcf03a3d937c3a4985f22d48ea3cad
c4710n <c4710n@users.noreply.github.com>
2023-10-13 22:32:25 +0800

    fix: pull the image before checking healthcheck declaration

--- lib/docker_compizo/image.ex
+++ lib/docker_compizo/image.ex
@@ -2,7 +2,20 @@ defmodule DockerCompizo.Image do
   alias DockerCompizo.Context
   alias DockerCompizo.Command
 
+  def exist?(%Context{} = context, image) do
+    case Command.run(:batch, bin(context), ["image", "inspect", image], silent: true) do
+      {:ok, _} -> true
+      _ -> false
+    end
+  end
+
+  def pull(%Context{} = context, image) do
+    {:ok, _} = Command.run(:batch, bin(context), ["image", "pull", "-q", image])
+  end
+
   def has_healthcheck?(%Context{} = context, image) do
+    if !exist?(context, image), do: pull(context, image)
+
     {:ok, json_string} =
       Command.run(:batch, bin(context), ["image", "inspect", "--format", "{{json .ContainerConfig}}", image])
 
